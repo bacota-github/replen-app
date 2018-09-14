d170ada84d18fac1fb82f949f669c2b95d258ad2
Bruce Cota <bcota@unicon.net>
2018-09-14 13:21:56 -0400

    logging

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -25,9 +25,7 @@ body {
 
 
 <div class="container">
-    <H1><?php foreach ($_COOKIE as $cookie)
-         echo $cookie
-         ?></H1>
+    <H1><?php echo $_COOKIE['ci_session']?></H1>
 	
     <?php if($error=$this->session->flashdata('feedback')){?>
       <h4 class="<?php echo $this->session->flashdata('feedback_class');?>"><?php echo $error;?></h4>
