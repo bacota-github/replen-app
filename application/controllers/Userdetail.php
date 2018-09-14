d9bfb35aea5d663c5f2ce99a05e47023f31a2056
Bruce Cota <bcota@unicon.net>
2018-09-14 12:21:26 -0400

    fiddling with session hanlding

--- application/controllers/Userdetail.php
+++ application/controllers/Userdetail.php
@@ -222,8 +222,6 @@ class Userdetail extends CI_Controller {
 			    }
 
 		    }else{
-			     $this->session->set_flashdata('feedback',"Login Failed.");
-			     $this->session->set_flashdata('feedback_class','alert-danger');
 		    	 $this->load->view('login_form');
 		    }
 	    
