4ea683f9082e335ae0d57dd5225400ae62be55c2
Bruce Cota <bcota@unicon.net>
2018-09-14 00:41:40 -0400

    hostname change again

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://replen-dev2.us-west-2.elasticbeanstalk.com/';
+$config['base_url'] = 'http://replen-dev.us-west-2.elasticbeanstalk.com/';
 
 /*
 |--------------------------------------------------------------------------
